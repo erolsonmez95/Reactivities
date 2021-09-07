25eb29cf2b84bb7f658004d8856b805695f02079
erolsonmez95 <erolsonmez1995@gmail.com>
2021-09-07 11:25:23 +0300

    ui improved

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -20,6 +20,17 @@ export default class ActivityStore {
 
     }
 
+    get groupedActvities(){
+        return Object.entries(
+            this.activitiesByDate.reduce((activities,activity)=>{
+                const date= activity.date;
+                activities[date]= activities[date] ? [...activities[date],activity]
+                : [activity];
+                return activities;
+            }, {} as {[key: string]:Activity[]})
+        )
+    }
+
     loadActivities = async () => {
         this.loadingInitial=true;
         try {
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+import 'react-calendar/dist/Calendar.css';
 import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
