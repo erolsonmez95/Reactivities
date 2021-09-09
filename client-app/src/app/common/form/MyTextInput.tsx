8665eeaf79f395fb9a83a8d4235bd6a3177a100b
erolsonmez95 <erolsonmez1995@gmail.com>
2021-09-09 19:12:49 +0300

    login & register handled on client side

--- client-app/src/app/common/form/MyTextInput.tsx
+++ client-app/src/app/common/form/MyTextInput.tsx
@@ -6,6 +6,7 @@ interface Props{
     placeholder:string;
     name: string;
     label?:string;
+    type?: string;
 }
 export default function MyTexInput(props:Props){
     const [field,meta]= useField(props.name);
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -12,9 +12,27 @@ import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/errors/NotFound';
 import ServerError from '../../features/errors/ServerError';
+import LoginForm from '../../features/users/LoginForm';
+import { useStore } from '../stores/store';
+import { useEffect } from 'react';
+import LoadingComponent from './LoadingComponent';
+import ModalContainer from '../common/modals/ModalContainer';
+import RegisterForm from '../../features/users/RegisterForm';
 
 function App() {
   const location = useLocation();
+  const {commonStore,userStore} = useStore();
+  useEffect(()=>{
+    if (commonStore.token) {
+      userStore.getUser().finally(()=> commonStore.setAppLoaded());
+
+    }else{
+      commonStore.setAppLoaded();
+    }
+  },[commonStore,userStore])
+
+  if(!commonStore.appLoaded) return<LoadingComponent 
+  content='Loading App...'/>
   return (
     <>
       <ToastContainer
@@ -29,6 +47,7 @@ function App() {
         draggable
         pauseOnHover
       />
+      <ModalContainer />
 
       <Route exact path='/' component={HomePage} />
       <Route
@@ -45,6 +64,9 @@ function App() {
                 <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
                 <Route path='/errors' component={TestErrors} />
                 <Route path='/server-error' component={ServerError} />
+                <Route path='/login' component={LoginForm} />
+                <Route path='/register' component={RegisterForm} />
+                
                 <Route component={NotFound} />
 
               </Switch>
