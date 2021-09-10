54d0f54bf1db124bbcd6952ca5362115fd6491b7
erolsonmez95 <erolsonmez1995@gmail.com>
2021-09-10 20:23:33 +0300

    Attendance feautres implemented on frontend

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,24 +12,15 @@ import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import { categoryOptions } from '../../../app/common/form/options/categoryOptions';
 import MyDateInput from '../../../app/common/form/MyDateInput';
-import { Activity } from '../../../app/models/activity';
+import {  ActivityFormValues } from '../../../app/models/activity';
 
 
 export default observer(function ActivityForm() {
     const history = useHistory();
     const { activityStore } = useStore();
-    const { loadingInitial, createActivity, updateActivity, loading, loadActivity } = activityStore;
+    const { loadingInitial, createActivity, updateActivity, loadActivity } = activityStore;
     const { id } = useParams<{ id: string }>();
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required!'),
@@ -41,12 +32,12 @@ export default observer(function ActivityForm() {
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!))
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
     }, [id, loadActivity]);
 
-    function handleFormSubmit(activity:Activity){
+    function handleFormSubmit(activity:ActivityFormValues){
 
-        if(activity.id.length===0){
+        if(!activity.id){
             let newActivity= {
                 ...activity,
                 id:uuid()
@@ -89,7 +80,7 @@ export default observer(function ActivityForm() {
                         <MyTexInput placeholder='Venue' name='venue' />
                         <Button 
                         disabled={isSubmitting || !dirty || !isValid}
-                        loading={loading} floated='right' positive type='submit' content='Submit' />
+                        loading={isSubmitting} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
 
                     </Form>
